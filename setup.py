65ed4679ad41c7a6d2ad4b597bee1433f663a875
Telofy <git@yu-shin.de>
2014-05-07 18:33:04 +0200

    OParl Validator as dependency

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     ),
     install_requires=[
         'setuptools',
+        'oparlvalidator',
         'psycopg2==2.5.2',
         'django-admin-decorators==0.1',
         'Django==1.6.4',
